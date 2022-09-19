a783b79b42c6c07df58d4cf92e99b88e90d1590c
Gota7 <gotaseven@gmail.com>
2022-09-19 13:39:55 -0400

    Finish Repeater

--- src/input/repeater.h
+++ src/input/repeater.h
@@ -2,16 +2,40 @@
 
 #include "controller.h"
 
+#include "../jsystem.h"
+
 // Trigger an activation every X seconds.
+template<typename T, size_t U>
 struct IRepeater
 {
     float timePeriod; // How much time to pass until event occurs.
     float lastTime; // Time since last update.
+    IControllerAssignment<T, U>& assignment; // Assignment to poll.
+    T button; // Button to pull.
+    bool resetIfReleased = true; // Reset counter if released.
 
     // Constructor.
-    IRepeater();
+    IRepeater(IControllerAssignment<T, U>& assignment, T button) : assignment(assignment), button(button)
+    {
+        lastTime = (float)JFrame::currentFrame;
+    }
 
     // Get the input value.
-    float Poll();
+    float Poll()
+    {
+        float val = assignment.ButtonDown(button);
+        if (val == 0.0f)
+        {
+            if (resetIfReleased) lastTime = (float)JFrame::currentFrame;
+            return 0.0f;
+        }
+        float diff = (float)JFrame::currentFrame - lastTime;
+        if (diff > timePeriod)
+        {
+            lastTime = JFrame::currentFrame + diff;
+            return val;
+        }
+        return 0.0f;
+    }
 
 };
