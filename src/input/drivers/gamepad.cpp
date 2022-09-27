07ad2443745584ea8f6f2c0b00bac094457aae2f
Gota7 <gotaseven@gmail.com>
2022-09-26 22:14:53 -0400

    Gamepads Almost Done
    More gamepad progress.

--- src/input/drivers/gamepad.cpp
+++ src/input/drivers/gamepad.cpp
@@ -2,12 +2,54 @@
 
 IDriverGamepad* IDriverGamepad::globalGamepads[NUM_PLAYERS];
 
-IDriverGamepad::IDriverGamepad(GLFWwindow* window) : window(window)
+void IDriverGamepad::Init()
 {
+    int playerNum = 0;
+    for (int jid = GLFW_JOYSTICK_1; jid <= GLFW_JOYSTICK_LAST; jid++)
+    {
+        if (globalGamepads[playerNum]->TryConnectJoystick(jid))
+        {
+            playerNum++;
+            if (playerNum >= NUM_PLAYERS) return;
+        }
+    }
 }
 
 void IDriverGamepad::OnConnectDisconnect(int jid, int event)
 {
+    if (event == GLFW_CONNECTED)
+    {
+
+        // First scan for matching GUID.
+        for (int i = 0; i < NUM_PLAYERS; i++)
+        {
+            if (globalGamepads[i]->lastGUID == glfwGetJoystickGUID(jid))
+            {
+                if (globalGamepads[i]->TryConnectJoystick(jid)) return;
+            }
+        }
+
+        // Second scan for any spot available.
+        for (int i = 0; i < NUM_PLAYERS; i++)
+        {
+            if (globalGamepads[i]->TryConnectJoystick(jid)) return;
+        }
+
+    }
+    else if (event == GLFW_DISCONNECTED)
+    {
+
+        // Remove joysticks with JID.
+        for (int i = 0; i < NUM_PLAYERS; i++)
+        {
+            if (globalGamepads[i]->connected && globalGamepads[i]->jid == jid) globalGamepads[i]->DisconnectJoystick();
+        }
+
+        // TODO: REMOVE JID JOYSTICKS FROM AVAILABILITY QUEUE.
+
+        // TODO: SEARCH IF THERE ARE UNOCCUPIED JOYSTICKS AVAILABLE AND USE THEM! SMART IDEA WOULD BE TO HAVE A QUEUE OF AVAILABLE ONES.
+
+    }
 }
 
 bool IDriverGamepad::TryConnectJoystick(int jid)
@@ -27,7 +69,14 @@ bool IDriverGamepad::TryConnectJoystick(int jid)
         glfwGetJoystickButtons(jid, &buttonCount);
         name = std::string(glfwGetJoystickName(jid));
     }
-    // TODO: SETUP MAP IN ADVANCE!
+    for (int i = 0; i < axesCount; i++)
+    {
+        currInputs[i + AXES_START] = 0.0f;
+    }
+    for (int i = 0; i < buttonCount; i++)
+    {
+        currInputs[i + BUTTON_START] = 0.0f;
+    }
     this->jid = jid;
     connected = true;
     lastGUID = std::string(glfwGetJoystickGUID(jid));
@@ -40,18 +89,14 @@ void IDriverGamepad::DisconnectJoystick()
     // We need to mark as disconnected and erase any current inputs.
     // Nonexistant values are inserted to input map when needed so we should be safe to eras.
     connected = false;
-    // for (int i = 0; i < axesCount; i++)
-    // {
-    //     currInputs.erase(i + AXES_START);
-    // }
-    // for (int i = 0; i < buttonCount; i++)
-    // {
-    //     currInputs.erase(i + BUTTON_START);
-    // }
-    // for (int i = 0; i < hatCount; i++)
-    // {
-    //     currInputs.erase(i + HAT_START);
-    // }
+    for (int i = 0; i < axesCount; i++)
+    {
+        currInputs.erase(i + AXES_START);
+    }
+    for (int i = 0; i < buttonCount; i++)
+    {
+        currInputs.erase(i + BUTTON_START);
+    }
 }
 
 void IDriverGamepad::RegisterCallbacks()
@@ -61,4 +106,32 @@ void IDriverGamepad::RegisterCallbacks()
 
 void IDriverGamepad::Update()
 {
+    if (gamepadMode)
+    {
+        GLFWgamepadstate st;
+        if (glfwGetGamepadState(jid, &st))
+        {
+            for (int i = 0; i < axesCount; i++)
+            {
+                currInputs[i + AXES_START] = st.axes[i];
+            }
+            for (int i = 0; i < buttonCount; i++)
+            {
+                currInputs[i + BUTTON_START] = (float)st.buttons[i];
+            }
+        }
+    }
+    else
+    {
+        const float* axes = glfwGetJoystickAxes(jid, &axesCount);
+        for (int i = 0; i < axesCount; i++)
+        {
+            currInputs[i + AXES_START] = axes[i];
+        }
+        const unsigned char* buttons = glfwGetJoystickButtons(jid, &buttonCount);
+        for (int i = 0; i < buttonCount; i++)
+        {
+            currInputs[i + BUTTON_START] = (float)buttons[i];
+        }
+    }
 }
